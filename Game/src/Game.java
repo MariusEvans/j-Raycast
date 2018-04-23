77173d73b92bfe0752c4fd295edca0a504e7042b
MariusEvans <38615325+MariusEvans@users.noreply.github.com>
2018-04-23 18:28:50 +0100

    Update Game - Map, commenting, textures, FOV

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -11,8 +11,8 @@ public class Game extends JFrame implements Runnable
 	//runnable allows a thread to be run
 	
 	private static final long serialVersionUID = 1L;
-	public int mapWidth = 15;
-	public int mapHeight = 15;
+	public int mapWidth = 16;
+	public int mapHeight = 16;
 	private Thread thread; //allows multiple things to be done at once
 	private boolean running;
 	private BufferedImage image;
@@ -23,38 +23,41 @@ public class Game extends JFrame implements Runnable
 	
 	//0 represents no wall, all other numbers represent textures on walls
 	//1 is wood
-	//2 is red brick
-	//3 is bluestone
-	//4 is grey brick
+	//2 is greystone
+	//3 is redbrick
+	//4 is stonewall
 	public static int[][] map = 
 		{
-			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-			{1,0,0,0,1,2,3,4,0,0,0,0,0,0,1},
-			{1,0,2,0,0,0,0,0,0,0,0,0,0,0,2},
-			{1,0,0,0,0,0,0,0,3,0,0,0,0,0,2},
-			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-			{1,0,0,0,0,0,0,0,0,0,0,1,0,0,2},
-			{1,0,0,0,0,0,0,0,2,0,0,0,0,0,2},
-			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
-			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,0,0,0,1,4,0,0,0,0,0,0,4},
-			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,0,0,0,1,4,0,3,3,3,3,0,4},
-			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
-			{1,1,1,1,1,1,1,4,4,4,4,4,4,4,4}
+	       //1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+			{4,2,2,2,2,2,2,4,4,4,4,2,4,2,2,2},//1
+			{4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},//2
+			{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},//3
+			{4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},//4
+			{2,0,0,3,0,0,0,0,0,0,3,0,0,0,0,4},//5
+			{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},//5
+			{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},//7
+			{4,3,0,0,3,0,0,1,0,0,3,0,0,0,3,2},//8
+			{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},//9
+			{4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},//10
+			{4,0,0,0,3,0,0,0,0,0,3,0,0,0,0,4},//11
+			{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},//12
+			{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},//13
+			{4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},//14
+			{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},//15
+			{4,2,2,2,2,2,2,4,4,4,4,4,4,4,4,2}//16
 		};
+	
 	public Game() 
 	{
 		thread = new Thread(this);
 		image = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
-		pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+		pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData(); //connects pixels + image, any time the data values in pixels are changed, corresponding changes appear on image 
 		textures = new ArrayList<Texture>();
 		textures.add(Texture.wood);
-		textures.add(Texture.brick);
-		textures.add(Texture.bluestone);
+		textures.add(Texture.greystone);
+		textures.add(Texture.redbrick);
 		textures.add(Texture.stone);
-		camera = new Camera(4.5, 4.5, 1, 0, 0, -.66);
+		camera = new Camera(4.5, 4.5, 1, 0, 0, -0.80);
 		screen = new Screen(map, mapWidth, mapHeight, textures, 640, 480);
 		addKeyListener(camera);
 		setSize(640, 480);
@@ -87,10 +90,10 @@ public class Game extends JFrame implements Runnable
 	}
 	public void render() 
 	{
-		BufferStrategy bs = getBufferStrategy();
+		BufferStrategy bs = getBufferStrategy(); //used when rendering so that screen updates are smoother
 		if(bs == null) 
 		{
-			createBufferStrategy(3);
+			createBufferStrategy(3); //3D
 			return;
 		}
 		Graphics g = bs.getDrawGraphics();
@@ -100,7 +103,7 @@ public class Game extends JFrame implements Runnable
 	
 	public void run() 
 	{
-		long lastTime = System.nanoTime();
+		long lastTime = System.nanoTime(); //current time
 		final double ns = 1000000000.0 / 40.0;//40 times per second
 		double delta = 0;
 		requestFocus();
